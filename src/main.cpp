d728787d59cc7d741a2b58b29ef3355931c0f991
Adam Derylo <a.m.derylo@gmail.com>
2023-07-05 19:43:56 +0200

    Fix screen tearing

--- src/main.cpp
+++ src/main.cpp
@@ -31,15 +31,14 @@ class Pomodoro{
       _start_time = millis() / 1000;
       _is_break = false;
       _sessions_completed = 0;
-      _total_sessions = 1;
-      _time_per_break = 1 * 10;
-      _time_per_long_break = 2 * 10;
-      _time_per_session = 1 * 10;
+      _total_sessions = 4;
+      _time_per_break = 1 * 5;
+      _time_per_long_break = 2 * 5;
+      _time_per_session = 1 * 5;
       _remaining_time = _total_time = _time_per_session;
     }
 
     void tick(){
-      _lcd.clear();
       updateTime();
       if(_remaining_time == 0){
         changeState();
@@ -55,21 +54,26 @@ class Pomodoro{
 
   private:
     void printFocusInfo(){
-      uint8_t minutes = _remaining_time / 60;
-      uint8_t seconds = _remaining_time % 60;
       std::string focus_info = "Focus!" 
-        + std::to_string(minutes) + ":" + std::to_string(seconds); 
+        + getRemainingTime();
       _lcd.setCursor(0, 0);
       _lcd.print(focus_info.c_str());
     }
 
     void printBreakInfo(){
-      uint8_t minutes = _remaining_time / 60;
-      uint8_t seconds = _remaining_time % 60;
       std::string break_info = "Break!" 
-        + std::to_string(minutes) + ":" + std::to_string(seconds); 
+        + getRemainingTime();
       _lcd.setCursor(0, 0);
       _lcd.print(break_info.c_str());
+    }
+
+    std::string getRemainingTime(){
+      std::string result; 
+      uint8_t minutes = _remaining_time / 60;
+      uint8_t seconds = _remaining_time % 60;
+      result += std::to_string(minutes) + ":"; 
+      result += (seconds < 10) ? "0" + std::to_string(seconds) : std::to_string(seconds);
+      return result + " ";
     }
 
     void printProgressBar(){
@@ -79,8 +83,9 @@ class Pomodoro{
         _lcd.write((byte)0);
       }
 
-      if(full_blocks < _cols){
-        _lcd.setCursor(full_blocks, 1);
+      for(uint64_t i = full_blocks; i < _cols; i++){
+        _lcd.setCursor(i, 1);
+        _lcd.write((byte)5);
       }
     }
 
@@ -92,6 +97,7 @@ class Pomodoro{
     }
 
     void changeState(){
+      _lcd.clear();
       if(!_is_break){
         _sessions_completed++;
         if(_sessions_completed == _total_sessions){
@@ -114,7 +120,7 @@ class Pomodoro{
     void initCustomChars()
     {
       _lcd.createChar(0, fullBlock);
-      for(uint8_t i = 1; i < 5; i++)
+      for(uint8_t i = 1; i <= 5; i++)
       {
         for(uint8_t j = 0; j < 8; j++)
         {
