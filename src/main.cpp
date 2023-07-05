887871a5fc6c30500f762e74d9c97d0157bb149c
Adam Derylo <a.m.derylo@gmail.com>
2023-07-05 18:22:21 +0200

    custom chars

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <LiquidCrystal_I2C.h>
 #include <Arduino.h>
 
+byte fullBlock[8] = {
+    0b11111,
+    0b11111,
+    0b11111,
+    0b11111,
+    0b11111,
+    0b11111,
+    0b11111,
+    0b11111};
+
 class Pomodoro{
   public:
     Pomodoro() : _lcd(0x27, 16, 2) {
@@ -12,32 +22,63 @@ class Pomodoro{
     void init(){
       _lcd.init();
       _lcd.backlight();
-      _lcd.clear();
-      _lcd.setCursor(0, 0);
-      _lcd.print("Hello, world!");
-
+      initCustomChars();
     }
+
+    void start()
+    {
+      _start_time = millis();
+      _total_time = 25 * 60 * 1000;
+    }
+
     void tick(){
       _lcd.clear();
       _lcd.setCursor(0, 0);
       _lcd.print("Dupa!");
+      updateTime();
       printProgressBar();
     }
 
   private:
     void printProgressBar(){
-      for(uint8_t i = 0; i < _cols; i++){
+      uint8_t full_blocks = min(_remaining_time * _cols / _total_time, _cols);
+      for(uint8_t i = 0; i < full_blocks; i++){
         _lcd.setCursor(i, 1);
-        _lcd.print("X");
+        _lcd.write((byte)0);
+      }
+
+      if(full_blocks < _cols){
+        _lcd.setCursor(full_blocks, 1);
       }
     }
 
+    void updateTime()
+    {
+      _remaining_time = _total_time - (millis() - _start_time);
+    }
 
+    void initCustomChars()
+    {
+      _lcd.createChar(0, fullBlock);
+      for(uint8_t i = 1; i < 5; i++)
+      {
+        for(uint8_t j = 0; j < 8; j++)
+        {
+          fullBlock[j] = fullBlock[j] << 1;
+        }
+
+
+        _lcd.createChar(i, fullBlock);
+      }
+    }
 
     LiquidCrystal_I2C _lcd; 
     uint8_t _rows; 
     uint8_t _cols;
-    
+
+    uint64_t _remaining_time;
+    uint64_t _start_time;
+    uint64_t _total_time;
 }; 
 
 Pomodoro pomodoro;
