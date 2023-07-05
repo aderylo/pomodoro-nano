16ba0f15cd1b4c5680c8c6208b530b85716b5861
Adam Derylo <a.m.derylo@gmail.com>
2023-07-05 12:46:27 +0200

    moving text

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,27 @@
-//YWROBOT
-//Compatible with the Arduino IDE 1.0
-//Library version:1.1
 #include <LiquidCrystal_I2C.h>
 #include <Arduino.h>
 
-LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2); // I2C address 0x27, 16 column and 2 rows
 
 void setup()
 {
-  lcd.init();                      // initialize the lcd 
-  // Print a message to the LCD.
-  lcd.backlight();
-  lcd.setCursor(0,0);
-  lcd.print("Time is ticking!");
-  lcd.setCursor(0,1);
-  lcd.print("!!!");
+  lcd.init(); // initialize the lcd
+  lcd.backlight(); 
 }
-
 
 void loop()
 {
+  lcd.clear();                 // clear display
+  lcd.setCursor(0, 0);         // move cursor to   (0, 0)
+  lcd.print("Arduino");        // print message at (0, 0)
+  lcd.setCursor(2, 1);         // move cursor to   (2, 1)
+  lcd.print("GetStarted.com"); // print message at (2, 1)
+  delay(2000);                 // display the above for two seconds
+
+  lcd.clear();                  // clear display
+  lcd.setCursor(3, 0);          // move cursor to   (3, 0)
+  lcd.print("DIYables");        // print message at (3, 0)
+  lcd.setCursor(0, 1);          // move cursor to   (0, 1)
+  lcd.print("www.diyables.io"); // print message at (0, 1)
+  delay(2000);                  // display the above for two seconds
 }
