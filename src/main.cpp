6577999af337dad257b0ffbc765d5f984386e1cc
Adam Derylo <a.m.derylo@gmail.com>
2023-07-05 19:23:50 +0200

    State change

--- src/main.cpp
+++ src/main.cpp
@@ -29,13 +29,27 @@ class Pomodoro{
     void start()
     {
       _start_time = millis() / 1000;
-      _remaining_time = _total_time = 10 * 60;
+      _is_break = false;
+      _sessions_completed = 0;
+      _total_sessions = 1;
+      _time_per_break = 1 * 10;
+      _time_per_long_break = 2 * 10;
+      _time_per_session = 1 * 10;
+      _remaining_time = _total_time = _time_per_session;
     }
 
     void tick(){
       _lcd.clear();
       updateTime();
-      printFocusInfo(); 
+      if(_remaining_time == 0){
+        changeState();
+      }
+
+      if(_is_break)
+        printBreakInfo();
+      else
+        printFocusInfo(); 
+
       printProgressBar();
     }
 
@@ -47,6 +61,15 @@ class Pomodoro{
         + std::to_string(minutes) + ":" + std::to_string(seconds); 
       _lcd.setCursor(0, 0);
       _lcd.print(focus_info.c_str());
+    }
+
+    void printBreakInfo(){
+      uint8_t minutes = _remaining_time / 60;
+      uint8_t seconds = _remaining_time % 60;
+      std::string break_info = "Break!" 
+        + std::to_string(minutes) + ":" + std::to_string(seconds); 
+      _lcd.setCursor(0, 0);
+      _lcd.print(break_info.c_str());
     }
 
     void printProgressBar(){
@@ -64,7 +87,28 @@ class Pomodoro{
     void updateTime()
     {
       uint64_t current_time = millis() / 1000;
-      _remaining_time = _total_time - (current_time - _start_time);
+      uint64_t time_elapsed = current_time - _start_time;
+      _remaining_time = (_total_time > time_elapsed) ? (_total_time - time_elapsed) : 0;
+    }
+
+    void changeState(){
+      if(!_is_break){
+        _sessions_completed++;
+        if(_sessions_completed == _total_sessions){
+          _total_time = _time_per_long_break;
+          _sessions_completed = 0;
+        }
+        else{
+          _total_time = _time_per_break;
+        }
+      }
+      else{
+        _total_time = _time_per_session;
+      }
+      
+      _is_break = !_is_break;
+      _start_time = millis() / 1000;
+      _remaining_time = _total_time;
     }
 
     void initCustomChars()
@@ -88,6 +132,16 @@ class Pomodoro{
     uint64_t _remaining_time;
     uint64_t _start_time;
     uint64_t _total_time;
+
+    uint8_t _time_per_session;
+    uint8_t _time_per_break;
+    uint8_t _time_per_long_break;
+
+
+
+    bool _is_break;
+    uint8_t _total_sessions;
+    uint8_t _sessions_completed;
 }; 
 
 Pomodoro pomodoro;
